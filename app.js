67d1035c9f95b3136c3c53ce17d7e50be0d318e8
YeSol <whassup_man@naver.com>
2021-10-22 02:24:09 +0900

    CLICK_EVENT1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
 title.innerText = "HELLO";
 
@@ -6,4 +6,15 @@ function handleTitleClick() {
   title.style.color = "blue";
   console.log("Title was Clicked!");
 }
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
