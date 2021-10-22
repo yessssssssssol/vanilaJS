5292d3bb088dec036411141e62db89b97c725954
YeSol <whassup_man@naver.com>
2021-10-23 00:14:56 +0900

    CSS in JS

--- app.js
+++ app.js
@@ -1,73 +1,48 @@
-const h2 = document.querySelector("h2");
+const age = prompt("How old are you?"); //css적용 불가능
 
-function handleTitleClick() {
-  h2.innerText = "That was a right click!";
-  h2.style.color = "#e74c3c";
+console.log(age, parseInt(age));
+
+const age = parseInt(prompt("How old are you?"));
+console.log(isNaN(age)); //숫자가 아니다.
+
+const age = parseInt(prompt("How old are you?"));
+
+&& and
+|| or
+true || true === true;
+false || true === true;
+true || false === true;
+false || false === false;
+
+true && true === true
+false && true === false
+true && false === false
+false && false === false
+
+if (isNaN(age) || age < 0) {
+  console.log("Please write a number");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else {
+  console.log("You can't drink");
 }
+
+
+//CSS in JS
+
+const h1 = document.querySelector("h1");
 
 function handleMouseEnter() {
-  h2.innerText = "Mouse is here!";
-  h2.style.color = "#1abc9c";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseLeave() {
-  h2.innerText = "Mouse is gone!";
-  h2.style.color = "#3498db";
-}
-
-function handleWindowResize() {
-  h2.innerText = "You just resized!";
-  h2.style.color = "#9b59b6";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOOD");
-}
-
-h2.addEventListener("click", handleTitleClick);
-h2.addEventListener("mouseenter", handleMouseEnter);
-h2.addEventListener("mouseleave", handleMouseLeave); //더 선호
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-
-과제;
-
-// const title = document.querySelector("h2");
-
-// const superEventHandler = {
-//   handleTitleClick: function () {
-//     title.innerText = "That was a right click!";
-//     title.style.color = "#e74c3c";
-//   },
-
-//   handleMouseEnter: function () {
-//     title.innerText = "Mouse is here!";
-//     title.style.color = "#1abc9c";
-//   },
-
-//   handleMouseLeave: function () {
-//     title.innerText = "Mouse is gone!";
-//     title.style.color = "#3498db";
-//   },
-
-//   handleWindowResize: function () {
-//     title.innerText = "You just resized!";
-//     title.style.color = "#9b59b6";
-//   }
-// };
-
-// title.addEventListener("click", superEventHandler.handleTitleClick);
-// title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-// title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-// window.addEventListener("resize", superEventHandler.handleWindowResize);
+h1.addEventListener("click", handleMouseEnter);
