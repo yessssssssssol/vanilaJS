53bd1fb6d26cc477e14e1ea489b0e68e878d1a45
YeSol <whassup_man@naver.com>
2021-10-26 21:29:01 +0900

    Login with HTML

--- app.js
+++ app.js
@@ -7,15 +7,15 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("Please write your name!!");
-  } else if (username.length > 15) {
-    alert("Your name is too long.");
-  }
-  console.log("Hello!", loginInput.value);
-  console.log("click!!!");
-}
+// function onLoginBtnClick() {
+//   const username = loginInput.value;
+//   if (username === "") {
+//     alert("Please write your name!!");
+//   } else if (username.length > 15) {
+//     alert("Your name is too long.");
+//   }
+//   console.log("Hello!", loginInput.value);
+//   console.log("click!!!");
+// }
 
 loginButton.addEventListener("click", onLoginBtnClick);
