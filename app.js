758eb03b5410c7476e9fb30a194b8d83317913c7
Hailey_ys <whassup_man@naver.com>
2022-07-04 23:46:37 +0900

    Add addEventListener

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-//case1
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector('#login-form input');
+const loginInput = document.querySelector('#login-form input');
 
-//case2
-// const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
-
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("Please write your name!!");
-  } else if (username.length > 15) {
-    alert("Your name is too long.");
-  }
-  console.log("Hello!", loginInput.value);
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
-
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener('submit', onLoginSubmit);
